5fdb28c555d9aacdb1556c5ae2d3a4eb1168434b
Pulipuli Chen <pulipuli.chen@gmail.com>
2023-06-20 23:55:51 +0800

    Tue Jun 20 11:55:51 PM CST 2023

--- app/blog.pulipuli.info/FeedSimple.js
+++ app/blog.pulipuli.info/FeedSimple.js
@@ -146,12 +146,14 @@ let modifyContent = function ($, container, url, title) {
 
     // text.push('----\n<br />\n<br />看看網頁版全文 ⇨ ' + title + '\n<br />' + url)
     // text.push('看看網頁版全文 ⇨ ' + title + '\n<br />')
-    text.push('----\n<br />\n<br />看看網頁版全文 ⇨ ' + title + '\n<br />')
+    // text.push('----\n<br />\n<br />看看網頁版全文 ⇨ ' + title + '\n<br />')
+    text.push('----\n<br />\n<br />看看網頁版全文 ⇨ ' + title + '\n<br />' + url)
   }
   else {
     // text.push('----\n<br />\n<br />繼續閱讀 ⇨ ' + title + '\n<br />' + url)
     // text.push('繼續閱讀 ⇨ ' + title + '\n<br />')
-    text.push('----\n<br />\n<br />繼續閱讀 ⇨ ' + title + '\n<br />')
+    // text.push('----\n<br />\n<br />繼續閱讀 ⇨ ' + title + '\n<br />')
+    text.push('----\n<br />\n<br />繼續閱讀 ⇨ ' + title + '\n<br />' + url)
   }
   
   // ------------
