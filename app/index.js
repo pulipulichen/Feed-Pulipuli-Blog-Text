d348b67307aad13e63735493820fa831ed0f51b5
Pulipuli Chen <pulipuli.chen@gmail.com>
2023-06-23 00:57:57 +0800

    Fri Jun 23 12:57:57 AM CST 2023

--- app/index.js
+++ app/index.js
@@ -2,12 +2,14 @@ const FeedSimple = require('./blog.pulipuli.info/FeedSimple.js')
 const FeedTiny = require('./blog.pulipuli.info/FeedTiny.js')
 const FeedPinterest = require('./blog.pulipuli.info/FeedPinterest.js')
 const FeedGist = require('./blog.pulipuli.info/FeedGist.js')
+// const t = require('../[trash/20230621-0130/t.js')
 
 let main = async () => {
   await FeedSimple()
   await FeedTiny()
   await FeedPinterest()
   await FeedGist()
+  // t()
 }
 main()
 
