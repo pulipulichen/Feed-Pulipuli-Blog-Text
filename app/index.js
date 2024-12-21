de35c9ed4363556dcce7fa9a08ed13e277583f8d
thinkpad-l13-yoga <pulipuli.chen@gmail.com>
2024-12-22 02:17:58 +0800

    qwqwqw

--- app/index.js
+++ app/index.js
@@ -3,6 +3,7 @@ const FeedTiny = require('./blog.pulipuli.info/FeedTiny.js')
 const FeedPinterest = require('./blog.pulipuli.info/FeedPinterest.js')
 const FeedGist = require('./blog.pulipuli.info/FeedGist.js')
 const FeedThreads = require('./blog.pulipuli.info/FeedThreads.js')
+const FeedAbstract = require('./blog.pulipuli.info/FeedAbstract.js')
 // const t = require('../[trash/20230621-0130/t.js')
 
 let main = async () => {
@@ -11,6 +12,7 @@ let main = async () => {
   await FeedPinterest()
   await FeedGist()
   await FeedThreads()
+  await FeedAbstract()
   // t()
 }
 main()
